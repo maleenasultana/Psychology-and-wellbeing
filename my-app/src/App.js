1e917167b22fa9b73a4e28e5567c19af9b255453
Maleena <maleena.sohail@gmail.com>
2024-09-28 00:26:13 +0530

    first commit

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './Components/Navbar'
+import Banner from './Components/Banner'
+import Contactus from "./Components/Contactus"
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+     <Banner/>
+     {/* <Contactus/> */}
+    </div>
+  )
+}
+
+export default App
